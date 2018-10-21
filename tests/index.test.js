dbe053c8df355579e9d28e9807bc5558cdc9c47d
Elian Gutierrez <eliangu3600103@gmail.com>
2018-10-21 16:19:50 -0400

    test: fix

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,5 +3,5 @@ const usbDevices = require('../lib')
 
 test('Get usb devices', async t => {
     const devices = await usbDevices.getDevices()
-    t.true(typeof devices === 'object')
+    t.true(Array.isArray(devices))
 })
