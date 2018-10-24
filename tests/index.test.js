e84d2793581b340fee047650595c68e2bf378950
Elian Gutierrez <eliangu3600103@gmail.com>
2018-10-23 20:11:51 -0400

    chore: rewrote tests with jest

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,38 +1,94 @@
-const { test } = require('ava')
-const usbDevices = require('../lib')
+const { getDevices, findByBusNumber, support } = require('../lib')
 
-test('Get usb devices', async t => {
-    const devices = await usbDevices.getDevices()
-    t.true(Array.isArray(devices))
+test('Get usb devices', async () => {
+    const devices = await getDevices()
+    expect(Array.isArray(devices)).toBe(true)
 })
 
-test('Find by bus number', async t => {
-    const expectedDevice = (await usbDevices.getDevices()).find(
-        ({ busNumber }) => busNumber === 1
-    )
-
-    const device = await usbDevices.findByBusNumber(1)
-
-    t.deepEqual(expectedDevice, device)
+test('Find device by is bus number', async () => {
+    const fakeDevices = [{ busNumber: 1 }, { busNumber: 2 }]
+    // mock function
+    jest.mock('../lib', () => {
+        getDevices: jest.fn(() =>
+            Promise.resolve([{ busNumber: 1 }, { busNumber: 2 }])
+        )
+    })
+    const device = await findByBusNumber(1)
+    expect(device.busNumber).toBe(fakeDevices[0].busNumber)
 })
 
-test('Get devices on a specific address', async t => {
-    const expectedDevice = (await usbDevices.getDevices()).filter(
-        device => device.address === 2
-    )
-    const device = await usbDevices.onAddress(2)
-    t.deepEqual(expectedDevice, device)
-})
-
-test('Get devices that support a specific usb version', async t => {
-    const expectedDevices = (await usbDevices.getDevices()).filter(
-        ({
+test('Find devices that support a specific usb version', async () => {
+    const fakeDevices = [
+        {
             deviceDescriptor: {
-                supportedUsbVersion: { majorVersion, minorVersion },
+                supportedUsbVersion: { majorVersion: 3, minorVersion: 0 },
             },
-        }) => majorVersion <= 2 && minorVersion >= 2
+        },
+        {
+            deviceDescriptor: {
+                supportedUsbVersion: { majorVersion: 2, minorVersion: 0 },
+            },
+        },
+        {
+            deviceDescriptor: {
+                supportedUsbVersion: { majorVersion: 0, minorVersion: 0 },
+            },
+        },
+        {
+            deviceDescriptor: {
+                supportedUsbVersion: { majorVersion: 1, minorVersion: 1 },
+            },
+        },
+    ]
+    // mock the function
+    jest.mock('../lib', () => {
+        getDevices: jest.fn(() =>
+            Promise.resolve([
+                {
+                    deviceDescriptor: {
+                        supportedUsbVersion: {
+                            majorVersion: 3,
+                            minorVersion: 0,
+                        },
+                    },
+                },
+                {
+                    deviceDescriptor: {
+                        supportedUsbVersion: {
+                            majorVersion: 2,
+                            minorVersion: 0,
+                        },
+                    },
+                },
+                {
+                    deviceDescriptor: {
+                        supportedUsbVersion: {
+                            majorVersion: 0,
+                            minorVersion: 0,
+                        },
+                    },
+                },
+                {
+                    deviceDescriptor: {
+                        supportedUsbVersion: {
+                            majorVersion: 1,
+                            minorVersion: 1,
+                        },
+                    },
+                },
+            ])
+        )
+    })
+
+    const devices = (await support(2)).map(({ deviceDescriptor }) => {
+        deviceDescriptor
+    })
+
+    const expectedDevices = fakeDevices.filter(
+        ({ deviceDescriptor: { supportedUsbVersion } }) =>
+            supportedUsbVersion.majorVersion <= 2 &&
+            supportedUsbVersion.minorVersion >= 2
     )
 
-    const devices = await usbDevices.support(2)
-    t.deepEqual(devices, expectedDevices)
+    expect(devices).toEqual(expectedDevices)
 })
